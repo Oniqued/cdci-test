0e4dd423391a2ef38e740051c7be30d8510286d8
김수현 <98441203+Oniqued@users.noreply.github.com>
2024-01-26 17:13:06 +0900

    Delete src/main/java/com/example/testrds/service/AuthService.java

--- src/main/java/com/example/testrds/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.example.testrds.service;
-
-import com.example.testrds.domain.Member;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface AuthService {
-    void addMember(Member member);
-}
